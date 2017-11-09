74ff0c7967fc93622bb171b8d1a44a9747262539
shan <shancse3333@gmail.com>
2017-11-09 15:43:45 +0530

    asasas

--- ChatService.java
+++ ChatService.java
@@ -23,7 +23,7 @@ public class ChatService {
 			if (clientMes.equals("exit"))
 				observer.onCompleted();
 		}
-//comment added by shan for new pulling - sri also updating the same file on machines
+//comment added by shan for new pulling - sri also updating the same file on machiness
   }
 
 }
